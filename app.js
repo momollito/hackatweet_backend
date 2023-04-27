cb7c09ea99535da07e6363b5f8359cc9f92de944
Mathis Rosset <rosset.mathis@gmail.com>
2023-04-27 11:47:17 +0200

    routes creation singin/up

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+require('./models/connection')
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require('uid2');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* SIGN UP  */
+
+router.post("/signup", async (req, res) => {
+  if (!checkBody(req.body, ["firstname", "username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  
+  const { firstname, username, password } = req.body;
+  
+  const findUser = await User.findOne({ username: username });
+  if (findUser) {
+    res.json({ result: false, error: "Username already used" });
+    return;
+  } else {
+    const token = uid2(32);
+    User.create({
+      firstname: firstname,
+      username: username,
+      password: password,
+      token:token,
+    });
+  }
+  res.json({ result: true, message: "User succesfully created" });
+  return;
 });
 
+/* SIGN IN  */
+
+router.post("/signin", async (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  const { username, password } = req.body;
+
+  const findUser = await User.findOne({
+    username: username,
+    password: password,
+  });
+
+  if (findUser) {
+    res.json({ result: true, message: "Login successful" });
+  } else {
+    res.json({ result: false, error: "Wrong password or username" });
+  }
+});
 module.exports = router;
